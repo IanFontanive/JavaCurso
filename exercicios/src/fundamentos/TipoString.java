0f4dcae07a0c5efdb122ad971521acb3de1b0372
IanFontanive <98621718+IanFontanive@users.noreply.github.com>
2025-12-17 16:11:22 -0300

    feat: Termino do assunto de tipo string

--- exercicios/src/fundamentos/TipoString.java
+++ exercicios/src/fundamentos/TipoString.java
@@ -16,5 +16,24 @@ public class TipoString {
 		IO.println(a.length());
 		System.out.println(a.equals("ryze blue"));
 		System.out.println(a.equalsIgnoreCase("ryze blue"));
+		
+		var nome = "Noah";
+		var sobrenome = "Baker";
+		var idade = 26;
+		var salario = 15360.86;
+		
+		String fichaDeDados = "Nome: " + nome + "\nSobrenome: "
+				+ sobrenome + "\nIdade: " + idade + 
+				"\nSalário: " + salario + "\n\n";
+		System.out.println(fichaDeDados);
+		System.out.printf("O senhor %s %s tem %d anos e ganha R$%.2f.",
+				nome, sobrenome, idade, salario);
+		
+		//Outros métodos de println
+		
+		System.out.println("Frase qualquer".contains("qual"));
+		IO.println("Frase qualquer".indexOf("qual"));
+		IO.println("Frase qualquer".substring(6));
+		IO.println("Frase qualquer".substring(6, 10));
 	}
 }
